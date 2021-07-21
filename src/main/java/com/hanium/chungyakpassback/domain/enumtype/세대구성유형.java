fbd8ce75f7de7b47f569741cf252e828e6cd5709
jiwoo-jus <parkjiwoo9980@gmail.com>
2021-07-21 23:02:02 +0900

    enum타입 추가 (숫자 들어있는 값들)

---
+++ src/main/java/com/hanium/chungyakpassback/domain/enumtype/세대구성유형.java
@@ -0,0 +1,5 @@
+package com.hanium.chungyakpassback.domain.enumtype;
+
+public enum 세대구성유형 {
+    세세대이상, 한부모가족
+}
---
+++ src/main/java/com/hanium/chungyakpassback/domain/enumtype/순위.java
@@ -0,0 +1,5 @@
+package com.hanium.chungyakpassback.domain.enumtype;
+
+public enum 순위 {
+    일순위, 이순위
+}
