fa41ae37e8b65daa2f110d0d3b3033ee21cdee2b
jieunee2 <980lje@naver.com>
2021-07-22 03:00:55 +0900

    1. 자산보유기준 entity 및 repository 생성 2. RepositoryTest_std에서 자산보유기준1,2,3 객체 생성 후 h2에 값 insert 성공 확인(Auto_Increment 되는 것도 확인)

---
+++ src/main/java/com/hanium/chungyakpassback/repository/자산보유기준repository.java
@@ -0,0 +1,7 @@
+package com.hanium.chungyakpassback.repository;
+
+import com.hanium.chungyakpassback.domain.standard.자산보유기준;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface 자산보유기준repository extends JpaRepository<자산보유기준, Long> {
+}
--- src/test/java/com/hanium/chungyakpassback/repository/RepositoryTest_std.java
+++ src/test/java/com/hanium/chungyakpassback/repository/RepositoryTest_std.java
@@ -1,13 +1,11 @@
 package com.hanium.chungyakpassback.repository;
 
-import com.hanium.chungyakpassback.domain.enumtype.여부;
-import com.hanium.chungyakpassback.domain.enumtype.지역_레벨1;
-import com.hanium.chungyakpassback.domain.enumtype.지역_레벨2;
-import com.hanium.chungyakpassback.domain.enumtype.특별공급유형;
+import com.hanium.chungyakpassback.domain.enumtype.*;
 import com.hanium.chungyakpassback.domain.input.세대;
 import com.hanium.chungyakpassback.domain.input.세대구성원;
 import com.hanium.chungyakpassback.domain.input.주소;
 import com.hanium.chungyakpassback.domain.standard.월평균소득기준;
+import com.hanium.chungyakpassback.domain.standard.자산보유기준;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,5 +25,12 @@ public class RepositoryTest_std {
     void 엔티티생성() {
         월평균소득기준 월평균소득기준1 = new 월평균소득기준(여부.y, 특별공급유형.신혼부부, null, 여부.n, 0, 100, 0, 6030160, 0, 0, 0, 0);
         em.persist(월평균소득기준1);
+
+        자산보유기준 자산보유기준1 = new 자산보유기준(자산유형.건물, 215500);
+        자산보유기준 자산보유기준2 = new 자산보유기준(자산유형.토지, 215500);
+        자산보유기준 자산보유기준3 = new 자산보유기준(자산유형.자동차, 34960);
+        em.persist(자산보유기준1);
+        em.persist(자산보유기준2);
+        em.persist(자산보유기준3);
     }
 }
