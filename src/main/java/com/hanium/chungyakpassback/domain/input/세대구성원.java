9d893b8224c1ce23f871cb04a9c3cb8c0b33bf05
jiwoo-jus <parkjiwoo9980@gmail.com>
2021-07-21 22:15:26 +0900

    세대구성원 entity생성, 세대구성원 관련 RepositoryTest 코드 작성

--- src/main/java/com/hanium/chungyakpassback/domain/input/세대구성원.java
+++ src/main/java/com/hanium/chungyakpassback/domain/input/세대구성원.java
@@ -4,10 +4,12 @@ import com.hanium.chungyakpassback.domain.enumtype.여부;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Table
@@ -21,7 +23,13 @@ public class 세대구성원 {
     @JoinColumn(name = "세대id")
     private 세대 세대;
 
-    //배우자 세대구성원 id 자기참조 넣기
+    @OneToOne
+    @JoinColumn(name = "배우자_세대구성원id")
+    private 세대구성원 배우자;
+
+    @OneToOne(mappedBy = "배우자")
+    @JoinColumn(name = "세대구성원id")
+    private 세대구성원 상대배우자;
 
     @Column
     @Enumerated(EnumType.STRING)
@@ -31,34 +39,28 @@ public class 세대구성원 {
     private String 이름;
 
     @Column
-    private Date 생년월일; // 년-월 로 date format 하기
+    private LocalDate 생년월일;
+
+//    @Column
+//    @Enumerated(EnumType.STRING)
+//    private 여부 외국인여부;
 
     @Column
-    @Enumerated(EnumType.STRING)
-    private 여부 외국인여부;
+    private LocalDate 혼인신고일;
 
     @Column
-    private Date 혼인신고일;
+    private LocalDate 무주택시작일;
 
     @Column
-    private Date 무주택시작일;
+    private LocalDate 전입일자;
 
-    @Column
-    private Date 전입일자;
-
-    @Column
-    private Date 전출일자;
+//    @Column
+//    private LocalDate 전출일자;
 
     @Builder
-    public 세대구성원(세대 세대, 여부 세대주여부, String 이름, Date 생년월일, 여부 외국인여부, Date 혼인신고일, Date 무주택시작일, Date 전입일자, Date 전출일자) {
+    public 세대구성원(세대 세대, 여부 세대주여부, String 이름){
         this.세대 = 세대;
         this.세대주여부 = 세대주여부;
         this.이름 = 이름;
-        this.생년월일 = 생년월일;
-        this.외국인여부 = 외국인여부;
-        this.혼인신고일 = 혼인신고일;
-        this.무주택시작일 = 무주택시작일;
-        this.전입일자 = 전입일자;
-        this.전출일자 = 전출일자;
-    };
+    }
 }
--- src/test/java/com/hanium/chungyakpassback/repository/RepositoryTest.java
+++ src/test/java/com/hanium/chungyakpassback/repository/RepositoryTest.java
@@ -1,6 +1,8 @@
 package com.hanium.chungyakpassback.repository;
 
+import com.hanium.chungyakpassback.domain.enumtype.여부;
 import com.hanium.chungyakpassback.domain.input.세대;
+import com.hanium.chungyakpassback.domain.input.세대구성원;
 import com.hanium.chungyakpassback.domain.input.주소;
 import com.hanium.chungyakpassback.domain.enumtype.지역_레벨1;
 import com.hanium.chungyakpassback.domain.enumtype.지역_레벨2;
@@ -11,6 +13,7 @@ import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDate;
 
 @SpringBootTest
 @Transactional
@@ -29,10 +32,31 @@ public class RepositoryTest {
         세대 세대2 = new 세대(주소1);
         세대 세대3 = new 세대(주소2);
 
+        세대구성원 세대구성원1 = new 세대구성원(세대1, 여부.y, "장윤정");
+        세대구성원 세대구성원2 = new 세대구성원(세대3, 여부.y, "도경완");
+        세대구성원 세대구성원3 = new 세대구성원(세대1, 여부.n, "도연우");
+        세대구성원 세대구성원4 = new 세대구성원(세대2, 여부.y, "이효리");
+        세대구성원 세대구성원5 = new 세대구성원(세대2, 여부.n, "이상순");
+
+        세대구성원1.set배우자(세대구성원2);
+        세대구성원1.set혼인신고일(LocalDate.of(1995, 12, 31));
+        세대구성원1.set무주택시작일(LocalDate.of(2017, 06, 21));
+        세대구성원1.set전입일자(LocalDate.of(2020, 06, 21));
+        세대구성원2.set배우자(세대구성원1);
+        세대구성원2.set혼인신고일(LocalDate.of(1995, 12, 31));
+        세대구성원2.set무주택시작일(LocalDate.of(1970, 11, 28));
+        세대구성원2.set전입일자(LocalDate.of(2021, 02, 01));
+
+
         em.persist(주소1);
         em.persist(주소2);
         em.persist(세대1);
         em.persist(세대2);
         em.persist(세대3);
+        em.persist(세대구성원1);
+        em.persist(세대구성원2);
+        em.persist(세대구성원3);
+        em.persist(세대구성원4);
+        em.persist(세대구성원5);
     }
 }
