1f3b0837b6142c8e46ae4b5e8962ef4c0371eb23
jiwoo-jus <parkjiwoo9980@gmail.com>
2021-07-21 22:38:51 +0900

    repository명 오타 수정

--- src/main/java/com/hanium/chungyakpassback/repository/주소repository.java
+++ src/main/java/com/hanium/chungyakpassback/repository/주소repository.java
@@ -3,5 +3,5 @@ package com.hanium.chungyakpassback.repository;
 import com.hanium.chungyakpassback.domain.input.주소;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface 주소respository extends JpaRepository<주소, Long> {
+public interface 주소repository extends JpaRepository<주소, Long> {
 }
