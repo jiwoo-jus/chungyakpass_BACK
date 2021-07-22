2c6c1148db1b5a384002db1aa165659f8077280b
jiwoo-jus <parkjiwoo9980@gmail.com>
2021-07-22 17:13:23 +0900

    세대구성원, 회원 변경사항 적용하여 테스트코드 추가

--- src/test/java/com/hanium/chungyakpassback/repository/RepositoryTest.java
+++ src/test/java/com/hanium/chungyakpassback/repository/RepositoryTest.java
@@ -6,6 +6,7 @@ import com.hanium.chungyakpassback.domain.input.세대구성원;
 import com.hanium.chungyakpassback.domain.input.주소;
 import com.hanium.chungyakpassback.domain.enumtype.지역_레벨1;
 import com.hanium.chungyakpassback.domain.enumtype.지역_레벨2;
+import com.hanium.chungyakpassback.domain.input.회원;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,20 +33,22 @@ public class RepositoryTest {
         세대 세대2 = new 세대(주소1);
         세대 세대3 = new 세대(주소2);
 
-        세대구성원 세대구성원1 = new 세대구성원(세대1, 여부.y, "장윤정");
-        세대구성원 세대구성원2 = new 세대구성원(세대3, 여부.y, "도경완");
-        세대구성원 세대구성원3 = new 세대구성원(세대1, 여부.n, "도연우");
-        세대구성원 세대구성원4 = new 세대구성원(세대2, 여부.y, "이효리");
-        세대구성원 세대구성원5 = new 세대구성원(세대2, 여부.n, "이상순");
+        //필수 필드만 build, 나머지는 setter 이용
+        세대구성원 세대구성원1 = 세대구성원.builder().세대(세대1).세대주여부(여부.y).이름("장윤정").build();
+        세대구성원 세대구성원2 = 세대구성원.builder().세대(세대3).세대주여부(여부.y).이름("도경완").build();
+        세대구성원 세대구성원3 = 세대구성원.builder().세대(세대1).세대주여부(여부.n).이름("도연우").build();
+        세대구성원1.set세대구성원배우자(세대구성원2);
+        세대구성원2.set세대구성원배우자(세대구성원1);
+        //전체 필드 대상 build
+        세대구성원 세대구성원4 = 세대구성원.builder().세대(세대2).세대주여부(여부.y).이름("이효리").생년월일(LocalDate.of(1985, 03, 12)).build();
+        세대구성원 세대구성원5 = 세대구성원.builder().세대(세대2).이름("이상순").세대주여부(여부.n).build();
+        세대구성원4.set세대구성원배우자(세대구성원5);
+        세대구성원5.set세대구성원배우자(세대구성원4);
 
-        세대구성원1.set배우자(세대구성원2);
-        세대구성원1.set혼인신고일(LocalDate.of(1995, 12, 31));
-        세대구성원1.set무주택시작일(LocalDate.of(2017, 06, 21));
-        세대구성원1.set전입일자(LocalDate.of(2020, 06, 21));
-        세대구성원2.set배우자(세대구성원1);
-        세대구성원2.set혼인신고일(LocalDate.of(1995, 12, 31));
-        세대구성원2.set무주택시작일(LocalDate.of(1970, 11, 28));
-        세대구성원2.set전입일자(LocalDate.of(2021, 02, 01));
+        회원 회원1 = 회원.builder().이메일("jyj@naver.com").비밀번호("password1234").세대구성원본인(세대구성원1).장기복무중인군인여부(여부.n).build();
+
+
+
 
 
         em.persist(주소1);
@@ -58,5 +61,6 @@ public class RepositoryTest {
         em.persist(세대구성원3);
         em.persist(세대구성원4);
         em.persist(세대구성원5);
+        em.persist(회원1);
     }
 }
