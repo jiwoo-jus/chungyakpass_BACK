5a578979f67e7ad5e849b4e8616f1dfa155b5fa5
jiwoo-jus <parkjiwoo9980@gmail.com>
2021-07-26 00:08:00 +0900

    세대구성원_관계 entity, repository 생성 및 관련 RepositoryTest 코드 추가

---
+++ src/main/java/com/hanium/chungyakpassback/domain/input/세대구성원_관계.java
@@ -0,0 +1,40 @@
+package com.hanium.chungyakpassback.domain.input;
+
+import com.hanium.chungyakpassback.domain.enumtype.관계;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table
+public class 세대구성원_관계 {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "세대구성원_관계id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "회원id")
+    private 회원 회원;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "세대구성원id")
+    private 세대구성원 관계자_세대구성원;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private 관계 관계;
+
+
+    @Builder
+    public 세대구성원_관계(회원 회원, 세대구성원 관계자_세대구성원, 관계 관계) {
+        this.회원 = 회원;
+        this.관계자_세대구성원 = 관계자_세대구성원;
+        this.관계 = 관계;
+    }
+}
---
+++ src/main/java/com/hanium/chungyakpassback/repository/세대구성원_관계repository.java
@@ -0,0 +1,7 @@
+package com.hanium.chungyakpassback.repository;
+
+import com.hanium.chungyakpassback.domain.input.세대구성원_관계;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface 세대구성원_관계repository extends JpaRepository<세대구성원_관계, Long> {
+}
