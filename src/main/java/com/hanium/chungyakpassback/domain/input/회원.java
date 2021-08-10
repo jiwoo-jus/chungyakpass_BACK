82193bcbc6f06a8223460b92c26b2c67bbd20553
jieunee2 <980lje@naver.com>
2021-08-10 13:18:00 +0900

    장기복무군인여부 column <회원> -> <세대구성원>으로 이동

--- src/main/java/com/hanium/chungyakpassback/domain/input/회원.java
+++ src/main/java/com/hanium/chungyakpassback/domain/input/회원.java
@@ -1,20 +1,19 @@
 package com.hanium.chungyakpassback.domain.input;
 
 import com.hanium.chungyakpassback.domain.enumtype.여부;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name="입력_회원")
+@Table(name = "입력_회원")
 public class 회원 {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "회원id")
     private Long id;
 
@@ -28,15 +27,11 @@ public class 회원 {
     @Column
     private String 비밀번호;
 
-    @Column
-    @Enumerated(EnumType.STRING)
-    private 여부 장기복무중인군인여부;
 
     @Builder
-    public 회원(세대구성원 세대구성원본인, String 이메일, String 비밀번호, 여부 장기복무중인군인여부) {
+    public 회원(세대구성원 세대구성원본인, String 이메일, String 비밀번호) {
         this.세대구성원본인 = 세대구성원본인;
         this.이메일 = 이메일;
         this.비밀번호 = 비밀번호;
-        this.장기복무중인군인여부 = 장기복무중인군인여부;
     }
 }
