dcd8d1e9cc89cc7d38b1b828f3b5b8a4885f5228
jiwoo-jus <parkjiwoo9980@gmail.com>
2021-08-21 02:51:35 +0900

    ec2로 애플리케이션 실행 시 DB 초기작업 및 조회작업

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,35 @@
 INSERT INTO inp_user (USER_ID, EMAIL, PASSWORD, ACTIVATED) VALUES (1, 'admin@gmail.com', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 1);
 INSERT INTO inp_user (USER_ID, EMAIL, PASSWORD, ACTIVATED) VALUES (2, 'user@gmail.com', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 1);
 
-INSERT INTO AUTHORITY (AUTHORITY_NAME) values ('ROLE_USER');
-INSERT INTO AUTHORITY (AUTHORITY_NAME) values ('ROLE_ADMIN');
+INSERT INTO authority (AUTHORITY_NAME) values ('ROLE_USER');
+INSERT INTO authority (AUTHORITY_NAME) values ('ROLE_ADMIN');
 
-INSERT INTO USER_AUTHORITY (USER_ID, AUTHORITY_NAME) values (1, 'ROLE_USER');
-INSERT INTO USER_AUTHORITY (USER_ID, AUTHORITY_NAME) values (1, 'ROLE_ADMIN');
-INSERT INTO USER_AUTHORITY (USER_ID, AUTHORITY_NAME) values (2, 'ROLE_USER');
+INSERT INTO user_authority (USER_ID, AUTHORITY_NAME) values (1, 'ROLE_USER');
+INSERT INTO user_authority (USER_ID, AUTHORITY_NAME) values (1, 'ROLE_ADMIN');
+INSERT INTO user_authority (USER_ID, AUTHORITY_NAME) values (2, 'ROLE_USER');
+
+
+-- # ec2 rds
+-- DROP database chungyakpass;
+-- CREATE database chungyakpass;
+--
+-- USE chungyakpass;
+--
+-- INSERT INTO inp_user (USER_ID, EMAIL, PASSWORD, ACTIVATED) VALUES (1, 'admin@gmail.com', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 1);
+-- INSERT INTO inp_user (USER_ID, EMAIL, PASSWORD, ACTIVATED) VALUES (2, 'user@gmail.com', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 1);
+--
+-- INSERT INTO authority (AUTHORITY_NAME) values ('ROLE_USER');
+-- INSERT INTO authority (AUTHORITY_NAME) values ('ROLE_ADMIN');
+--
+-- INSERT INTO user_authority (USER_ID, AUTHORITY_NAME) values (1, 'ROLE_USER');
+-- INSERT INTO user_authority (USER_ID, AUTHORITY_NAME) values (1, 'ROLE_ADMIN');
+-- INSERT INTO user_authority (USER_ID, AUTHORITY_NAME) values (2, 'ROLE_USER');
+--
+-- SELECT * FROM authority ;
+-- SELECT * FROM inp_user ;
+-- SELECT * FROM user_authority ;
+-- SELECT * FROM inp_user_bankbook ;
+-- SELECT * FROM inp_address ;
+-- SELECT * FROM inp_house ;
+-- SELECT * FROM inp_house_member ;
+-- SELECT * FROM inp_house_member_relation ;
