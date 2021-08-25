26a85528bf3cbbf92858988308ade8f7dba52de5
jiwoo-jus <parkjiwoo9980@gmail.com>
2021-08-25 16:31:23 +0900

    삭제한 파일

--- src/main/java/com/hanium/chungyakpassback/repository/input/HouseMemberIncomeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hanium.chungyakpassback.repository.input;
-
-import com.hanium.chungyakpassback.entity.input.HouseMemberIncome;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HouseMemberIncomeRepository extends JpaRepository<HouseMemberIncome, Long> {
-}
