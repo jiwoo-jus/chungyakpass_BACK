0d87ad415180c1af37f7e6d2afe213e89addf87c
jieunee2 <980lje@naver.com>
2021-10-05 18:15:35 +0900

    optional 추가

--- src/main/java/com/hanium/chungyakpassback/repository/standard/IncomeRepository.java
+++ src/main/java/com/hanium/chungyakpassback/repository/standard/IncomeRepository.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 public interface IncomeRepository extends JpaRepository<Income, Long> {
     Optional<Income> findBySpecialSupply(SpecialSupply specialSupply);
 
-    Optional<Income> findBySpecialSupplyAndSupplyAndDualIncome(SpecialSupply specialSupply, Supply supply, Yn dualIncome);
+    Optional<Income> findBySpecialSupplyAndSupplyAndDualIncomeAndApplicationPublicHousingSpecialLaws(SpecialSupply specialSupply, Supply supply, Yn dualIncome, Yn applicationPublicHousingSpecialLaws);
 
     List<Income> findAllBySpecialSupply(SpecialSupply specialSupply);
 
