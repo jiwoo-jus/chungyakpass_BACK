9d327dad155eb30085a0813a61e879dfca1de3a5
jiwoo-jus <parkjiwoo9980@gmail.com>
2021-10-10 15:41:49 +0900

    corsFilter 잠시 주석

--- src/main/java/com/hanium/chungyakpassback/config/CorsConfig.java
+++ src/main/java/com/hanium/chungyakpassback/config/CorsConfig.java
@@ -1,29 +1,29 @@
-package com.hanium.chungyakpassback.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-@Configuration
-public class CorsConfig {
-
-   @Bean
-   public CorsFilter corsFilter() {
-      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-      CorsConfiguration config = new CorsConfiguration();
-      config.setAllowCredentials(true);
-      config.addAllowedOrigin("*");
-      config.addAllowedHeader("*");
-      config.addAllowedMethod("*");
-
-      source.registerCorsConfiguration("/account/**", config);
-      source.registerCorsConfiguration("/user/**", config);
-      source.registerCorsConfiguration("/verification/**", config);
-      source.registerCorsConfiguration("/point/**", config);
-
-      return new CorsFilter(source);
-   }
-
-}
+//package com.hanium.chungyakpassback.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//   @Bean
+//   public CorsFilter corsFilter() {
+//      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//      CorsConfiguration config = new CorsConfiguration();
+//      config.setAllowCredentials(true);
+//      config.addAllowedOrigin("*");
+//      config.addAllowedHeader("*");
+//      config.addAllowedMethod("*");
+//
+//      source.registerCorsConfiguration("/account/**", config);
+//      source.registerCorsConfiguration("/user/**", config);
+//      source.registerCorsConfiguration("/verification/**", config);
+//      source.registerCorsConfiguration("/point/**", config);
+//
+//      return new CorsFilter(source);
+//   }
+//
+//}
